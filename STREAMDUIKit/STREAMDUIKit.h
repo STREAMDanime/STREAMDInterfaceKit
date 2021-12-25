8eca8cca1a89ccffd1b13de7e5303a3f87c7745c
Jules Labador <juleslabador@gmail.com>
2021-12-24 19:09:15 -0800

    Initial Commit

---
+++ STREAMDUIKit/STREAMDUIKit.h
@@ -0,0 +1,18 @@
+//
+//  STREAMDUIKit.h
+//  STREAMDUIKit
+//
+//  Created by Jules Labador on 12/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STREAMDUIKit.
+FOUNDATION_EXPORT double STREAMDUIKitVersionNumber;
+
+//! Project version string for STREAMDUIKit.
+FOUNDATION_EXPORT const unsigned char STREAMDUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STREAMDUIKit/PublicHeader.h>
+
+
