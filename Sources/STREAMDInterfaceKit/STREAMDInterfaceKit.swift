2db4e3a4e5cbb3275dedf31b1c08e7edea0c12d1
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2022-01-11 22:38:34 -0800

    Added Custom Fonts, fixed TitleAndSubtitleView
    - Added STREAMD's custom fonts to the package
    - To load these fonts, `STREAMDInterfaceKit.registerFonts()` needs to be called in the AppDelegate when the app loads.
    - Fixed TitleAndSubtitleView not displaying any text

---
+++ Sources/STREAMDInterfaceKit/STREAMDInterfaceKit.swift
@@ -0,0 +1,30 @@
+//
+//  STREAMDInterfaceKit.swift
+//  
+//
+//  Created by David Barsamian on 1/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+public struct STREAMDInterfaceKit {
+    fileprivate static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
+              let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
+              let font = CGFont(fontDataProvider) else {
+                  fatalError("Couldn't create font from filename: \(fontName).\(fontExtension)")
+              }
+        var error: Unmanaged<CFError>?
+        CTFontManagerRegisterGraphicsFont(font, &error)
+        if let error = error {
+            print("🛑 ERROR: \(error.takeRetainedValue().localizedDescription)")
+        }
+    }
+    
+    public static func registerFonts() {
+        STREAMDFonts.Poppins.allCases.forEach { font in
+            registerFont(bundle: .module, fontName: font.fullName, fontExtension: font.fontExtension)
+        }
+    }
+}
