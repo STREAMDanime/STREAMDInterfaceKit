0766bffff692815eac3ab0345d6f35bea9e50bd1
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2022-01-11 22:38:34 -0800

    Added Custom Fonts
    - Added STREAMD's custom fonts to the package
    - To load these fonts, `STREAMDInterfaceKit.registerFonts()` needs to be called in the AppDelegate when the app loads.

--- Package.swift
+++ Package.swift
@@ -24,9 +24,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "STREAMDInterfaceKit",
-            dependencies: ["SnapKit"]),
+            dependencies: ["SnapKit"],
+            resources: [.process("Fonts")]),
         .testTarget(
             name: "STREAMDInterfaceKitTests",
-            dependencies: ["STREAMDInterfaceKit", "SnapKit"]),
+            dependencies: ["STREAMDInterfaceKit", "SnapKit"])
     ]
 )
