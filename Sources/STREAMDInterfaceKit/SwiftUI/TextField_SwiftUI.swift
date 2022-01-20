40a6d13cca2c176623860c382f0f3f17f309fb20
Jules Labador <juleslabador@gmail.com>
2022-01-19 22:14:18 -0800

    Added TextField_SwiftUI

---
+++ Sources/STREAMDInterfaceKit/SwiftUI/TextField_SwiftUI.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Jules Labador on 1/19/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    
+    private let title: String
+    private let placeholder: String
+    
+    public @State var text: String = ""
+    
+    public init(title: String, placeholder: String) {
+        self.title = title
+        self.placeholder = placeholder
+    }
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 16)
+                .padding(.horizontal, 12)
+                .font(Font.custom("Avenir-Heavy", size: 12))
+            TextField(placeholder, text: $email)
+                .disableAutocorrection(true)
+                .padding(.horizontal, 12)
+                .padding(.bottom, 12)
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
