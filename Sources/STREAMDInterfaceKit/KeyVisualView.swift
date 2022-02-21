926c2e39cc57e475f15dbf8cce513feb3ed7cb92
Jules Labador <juleslabador@gmail.com>
2022-02-21 13:45:59 -0800

    Added a placeholder view in the KeyVisualView when image is nil

--- Sources/STREAMDInterfaceKit/KeyVisualView.swift
+++ Sources/STREAMDInterfaceKit/KeyVisualView.swift
@@ -9,6 +9,13 @@ import UIKit
 
 public class KeyVisualView: UIImageView {
     
+    public override var image: UIImage? {
+        didSet {
+            guard image == nil else { return }
+            createEmptyImageLayout()
+        }
+    }
+    
     init() {
         super.init(frame: .zero)
         
@@ -25,5 +32,36 @@ public class KeyVisualView: UIImageView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private let imagePlaceholder: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "image-placeholder")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    private let emptyImageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Looks like we're missing an image for this anime. Don't worry, it'll be added soon!"
+        label.font = .STREAMDFonts.body
+        label.textColor = .STREAMDColors.secondaryText
+        label.numberOfLines = -1
+        return label
+    }()
+    private func createEmptyImageLayout() {
+        
+        addSubview(imagePlaceholder)
+        imagePlaceholder.snp.makeConstraints { make in
+            make.width.equalTo(300)
+            make.height.equalTo(200)
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(self.snp.centerY).offset(-16)
+        }
+        
+        addSubview(emptyImageLabel)
+        emptyImageLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(16)
+            make.right.equalToSuperview().offset(-16)
+            make.top.equalTo(self.snp.centerY)
+        }
+    }
     
 }
